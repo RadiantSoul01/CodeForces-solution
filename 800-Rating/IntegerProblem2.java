166bf8c3baaab9e75101e27a6ea3ae1145702400
Himanshu kumar <class.24.ds.36@gmail.com>
2026-04-16 20:52:39 +0530

    Add files via upload

---
+++ 800-Rating/IntegerProblem2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class IntegerProblem2 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        
+
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+
+            int n = sc.nextInt();  
+            sc.nextLine();
+            System.out.println(n);
+        }
+    }
+    
+}
